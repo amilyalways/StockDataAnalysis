8391801431c784047c82e7a59cd4ffca5508a303
emily <yuqingxihe@163.com>
2017-09-15 19:31:55 +0800

    try to rewrite save revenue

--- Data/StatsRevenue.py
+++ Data/StatsRevenue.py
@@ -89,15 +89,15 @@ class StatsRevenue:
 
 if __name__ == '__main__':
     S = StatsRevenue()
-    '''
-    condition = ["MID(Times,1,6)", "ComputeLantency", "IntervalNum", "InMuUpper", "lnLastPriceThreshold"]
+
+    condition = ["MID(Times,1,8)", "ComputeLantency", "IntervalNum", "InMuUpper", "lnLastPriceThreshold"]
     tables = ["revenue20170914", "revenue_cut20170914", "revenue_anti20170914"]
     for table in tables:
         df = S.stats_revenue(table, condition)
         IM = ImExport(S.db)
-        IM.save_df_csv(df, "/home/emily/桌面/stockResult/stats20170914/", "stats_" + table +".csv")
+        IM.save_df_csv(df, "/home/emily/桌面/stockResult/stats20170915/", "stats_" + table +".csv")
 
-    '''
+
 
 
 
