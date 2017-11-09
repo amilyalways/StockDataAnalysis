3ef3ca07a8c7c119c5fe1ca6e08794bbde75e16b
emily <yuqingxihe@163.com>
2017-11-09 16:35:09 +0800

    add FileToMysql

--- Utility/RewriteFile.py
+++ Utility/RewriteFile.py
@@ -1,4 +1,4 @@
-
+# -*- coding: utf-8 -*-
 def rewriteFile(path, filename, new_rows):
 
     newfilename = "new" + filename
@@ -16,16 +16,27 @@ def rewriteFile(path, filename, new_rows):
 
                 id += 1
 
+def FiletoMysql(path, arr_filename):
+    for filename in arr_filename:
+        print "source " + path + "new" + filename
+
 
 if __name__ == '__main__':
-    path = "/Users/songxue/Desktop/"
-    arr_filename = ["3-2-(8-12).sql"]
+    path = "/home/emily/桌面/1106/"
+    arr_filename = ["1-1-(3-7).sql", "1-1-(8-12).sql", "1-2-(3-7).sql",
+                    "1-2-(8-12).sql", "2-1-(3-7)-missing.sql", "2-1-(8-12).sql",
+                    "2-2-(3-7).sql", "2-2-(8-12).sql", "3-1-(3-7)-missing.sql",
+                    "3-1-(8-12).sql", "3-2-(3-7).sql", "3-2-(8-12).sql"]
+    '''
     new_rows = {
-        24: "",
-        25: "CREATE TABLE IF NOT EXISTS `171016allpara` ("
+        21: "",
+        24: "CREATE TABLE IF NOT EXISTS `171016allpara` ("
     }
 
     for filename in arr_filename:
         print filename
         rewriteFile(path, filename, new_rows)
         print "********************************"
+    '''
+
+    FiletoMysql(path, arr_filename)
